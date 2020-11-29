8c3133319c43723a01e96ee17b4adc715c1c74d7
Nikolay <forsait27@gmail.com>
2020-11-29 16:42:26 +0300

    Вернул задание 6 (Класс Test)

--- Assets/Scenes/Scripts/Test.cs
+++ Assets/Scenes/Scripts/Test.cs
@@ -7,11 +7,41 @@ namespace NikolayTrofimov_Game
 {
     public class Test : MonoBehaviour
     {
-        private Transform _root;
-        
         void Start()
         {
-            _root = new GameObject("Root").transform;
+            string str = "некоторая длинная строка. Количество символов 59. А длина строки 68.";
+            int length = str.Length;
+            int count = str.StringSimbolsCount();
+            Debug.Log($"{count} {length}");
+
+            List<int> someList = new List<int>(20);
+            for (int i = 0; i < 20; i++)
+            {
+                someList.Add(UnityEngine.Random.Range(0, 10));
+                Debug.Log(someList[i]);
+            }
+            Dictionary<int, int> calculatedSomeList = GetNumberOfRecuring(someList);
+            foreach (KeyValuePair<int, int> someKey in calculatedSomeList)
+            {
+                Debug.Log($"елемент {someKey.Key} встречается {someKey.Value} раз");
+            }
+        }
+
+        private Dictionary<T, int> GetNumberOfRecuring<T>(ICollection<T> list)
+        {
+            Dictionary<T, int> count = new Dictionary<T, int>();
+            foreach (T val in list)
+            {
+                if (count.ContainsKey(val))
+                {
+                    count[val]++;
+                }
+                else
+                {
+                    count.Add(val, 1);
+                }
+            }
+            return count;
         }
     }
 }
